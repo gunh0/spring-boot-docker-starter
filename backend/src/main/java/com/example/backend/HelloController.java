e7caa1c54fbecbf15d857b98913660b8c522100e
gunh0 <devgun@kakao.com>
2022-09-03 11:29:42 +0900

    feat: add /first GET api

--- backend/src/main/java/com/example/backend/HelloController.java
+++ backend/src/main/java/com/example/backend/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.backend;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,6 +11,11 @@ public class HelloController {
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!!!";
+	}
+
+	@RequestMapping(value = "/first", method = RequestMethod.GET)
+	public String firstController(){
+		return "Hello, I'm first api.";
 	}
 
 }
